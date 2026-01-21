5ceef79df8e6dd9664aea97697ee8c25850f5c92
Andressagabriell <andressa_gabriel@outlook.com>
2026-01-21 00:38:47 -0300

    Configura variáveis de ambiente para deploy no Netlify

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ehzjetufkyevrpgentyr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoempldHVma3lldnJwZ2VudHlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4NTc5NDIsImV4cCI6MjA4NDQzMzk0Mn0.Bd9NDZZclYJhxqlXWW0wPHIUzBb5on_ZVktuym9s058'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
